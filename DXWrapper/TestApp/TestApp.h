16600aa8708e1d122442e72ed812a3ff0ee6d8e9
Nicholas Pastrano <pastranonicholas@gmail.com>
2016-03-31 20:51:05 -0500

    Added sub menu

--- DXWrapper/TestApp/TestApp.h
+++ DXWrapper/TestApp/TestApp.h
@@ -3,13 +3,16 @@
 
 #include "Utilities\Logger.h"
 #include "DxWrapper\DxWrapper.h"
+#include "Resource.h"
+#include <Windows.h>
 class Game : public DxWrapper
 {
 public:
    Game ();
    ~Game ();
-
+//WINDOW
 protected:
+   LPCTSTR resourceMenu (){return MAKEINTRESOURCE(IDC_TESTAPP); }//MAKEINTRESOURCE() creates a string ID from a resourcce ID
    bool gameInit ();
    void gameRun ();
    void gameExit ();
